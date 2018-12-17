bf238897de8c50b8e839c56ad33b523b15fa05a6
markodimitrije <markodimitrije@gmail.com>
2018-12-17 17:35:41 +0100

    horiz i vertical components + treba mi ako je vise od 3 da ide Stacker kao child u drugi Stacker

--- tryLeadLinkModularComponent/ViewController.swift
+++ tryLeadLinkModularComponent/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func btnTapped(_ sender: UIButton) {
-        btnIsTapped(sender: sender)
+    @IBAction func oneInRowTapped(_ sender: UIButton) {
+        oneInRowIsTapped(sender: sender)
     }
     
-    override func viewDidLoad() {    super.viewDidLoad()
-        
+    @IBAction func scenarioTapped(_ sender: UIButton) {
+        scenarioIsTapped(sender: sender)
     }
-
     
-    private func btnIsTapped(sender: UIButton) {
+    private func oneInRowIsTapped(sender: UIButton) {
         
         switch sender.tag {
             case 0: placeOneElement()
@@ -29,7 +28,21 @@ class ViewController: UIViewController {
         }
         
     }
-    //createLabelAndTextView(count
+    
+    private func scenarioIsTapped(sender: UIButton) {
+        
+        switch sender.tag {
+            case 0: buildScenario_1() // 3-1-7
+            case 1: buildScenario_2() // 3-6-3-1-2
+        default: break
+        }
+        
+        placeByTwoInRow(count: Int(sender.titleLabel!.text!) ?? 1)
+        
+    }
+    
+    // ovo je samo za testiranje, inace ces uzimati info iz jSON-a koji si dobio od APIController (web service layer) :
+    
     private func placeOneElement() {
         
         let components = createLabelAndTextView(count: 1)
@@ -52,6 +65,142 @@ class ViewController: UIViewController {
         self.view.addSubview(stackerView)
     }
     
+    // testiraj multiple in rows (2)
+    // 3-1-7
+    private func buildScenario_1() {
+        
+        let components_1 = createLabelAndTextView(count: 3)
+        let a = threeElementInRowView(components: components_1)
+        
+        let components_2 = createLabelAndTextView(count: 1)
+        let b = oneElementInRowView(components: components_2)
+        
+        let components_3 = createLabelAndTextView(count: 2)
+        let c = threeElementInRowView(components: components_3)
+        
+        let components = [a,b,c]
+        
+        let frame = getRect(forComponents: components)
+        let stackerView = ViewStacker.init(frame: frame , components: components)
+        self.view.addSubview(stackerView)
+        
+    }
+    
+    
+    
+    private func buildScenario_2() {
+        
+    }
+    
+    
+    
+    
+    private func oneElementInRowView(components: [UIView]) -> OneRowStacker {
+        
+        let rect = CGRect.init(origin: CGPoint.zero, size: CGSize.init(width: self.view.bounds.width, height: CGFloat.init(80)))
+        return OneRowStacker.init(frame: rect, components: components)!
+    }
+    
+    private func twoElementInRowView(components: [UIView]) -> OneRowStacker {
+        
+        let rect = CGRect.init(origin: CGPoint.zero, size: CGSize.init(width: self.view.bounds.width, height: CGFloat.init(80)))
+        return OneRowStacker.init(frame: rect, components: components)!
+    }
+    
+    private func threeElementInRowView(components: [UIView]) -> OneRowStacker {
+        
+        let rect = CGRect.init(origin: CGPoint.zero, size: CGSize.init(width: self.view.bounds.width, height: CGFloat.init(80)))
+        return OneRowStacker.init(frame: rect, components: components)!
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // treba ti logika za 2 items/row i 1 item/row (odd ili even):
+    
+    private func placeByTwoInRow(count: Int) {
+        // ako je odd -> treba ti da u poslednji stack ubacis 1 element
+        // u suprotnom -> ubacujes 2 elementa
+        
+        let numberOfStacks = 1 + (count - 1) / 2
+        
+        var stacks = [UIView]()
+        
+        if numberOfStacks == 1 {
+            switch count {
+                case 1:
+                    let s = getStackForSingleElement()
+                    stacks.append(s)
+                
+            case 2:
+                let s = getStackForSingleElement()
+                stacks.append(s)
+                
+//                case 2:
+//                    let s = getStackForTwoElements()
+//                    stacks.append(s)
+//                case 3:
+//                    let s = getStackForThreeElements()
+//                    stacks.append(s)
+            default: break
+            }
+        }
+        
+       //self.view.addSubview(<#T##view: UIView##UIView#>)
+        
+    }
+    
+    private func getStackForSingleElement() -> OneRowStacker {
+        
+        let components = createLabelAndTextView(count: 1)
+        let frame = getRect(forComponents: components)
+        let row = OneRowStacker.init(frame: frame, components: components)
+        return row!
+    }
+//
+//    private func getStackForTwoElements() -> UIStackView {
+//
+//    }
+//
+//    private func getStackForThreeElements() -> UIStackView {
+//
+////        let components = createLabelAndTextView(count: 3)
+////        let frame = getRect(forComponents: components)
+////        let stackerView = ViewStacker.init(frame: frame, components: components)
+////        return stackerView
+//    }
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     private func createLabelAndTextView() -> UIView {
         
         let or = CGPoint.zero
@@ -61,6 +210,8 @@ class ViewController: UIViewController {
         return v
     }
     
+    
+    
     private func createLabelAndTextView(count: Int) -> [UIView] {
         var result = [UIView]()
         for _ in 0...count-1 {
@@ -68,6 +219,8 @@ class ViewController: UIViewController {
         }
         return result
     }
+    
+    // calculate... sigurno nije na VC-u ....
     
     private func getRect(forComponents components: [UIView]) -> CGRect {
         
--- tryLeadLinkModularComponent/Views/OneRowStacker.swift
+++ tryLeadLinkModularComponent/Views/OneRowStacker.swift
@@ -18,8 +18,11 @@ class OneRowStacker: UIView {
         }
     }
     
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        loadViewFromNib()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -31,8 +34,13 @@ class OneRowStacker: UIView {
     convenience init?(frame: CGRect,
                      components: [UIView]) {
         guard components.count <= 3 else {return nil}
+        
         self.init(frame: frame)
+        
         self.components = components
+        
+        layoutComponents()
+        
     }
     
     func loadViewFromNib() {
@@ -45,14 +53,35 @@ class OneRowStacker: UIView {
         self.addSubview(view)
         
     }
+
+    override func awakeFromNib() {
+        layoutComponents()
+    }
     
     // imas case sa 1,2,3 components.
     private func layoutComponents() {
         
-        for view in components {
-            
-            stackView.addArrangedSubview(view)
+        print("components.count = \(components.count)")
         
+        for (index,view) in components.enumerated() {
+        
+            let mySize = view.bounds.size
+            
+            let newWidth = mySize.width * CGFloat(1)/CGFloat(components.count)
+            
+            let or = CGPoint.init(x: view.frame.origin.x + (CGFloat(8.0) + CGFloat(index) * newWidth),
+                                  y: view.frame.origin.y)
+            
+            let size = CGSize.init(width: newWidth, height: mySize.height)
+            
+            let rect = CGRect.init(origin: or, size: size)
+            
+            let resizedView = view
+            
+            resizedView.frame = rect
+            
+            stackView.addArrangedSubview(resizedView)
+            
         }
         
     }
