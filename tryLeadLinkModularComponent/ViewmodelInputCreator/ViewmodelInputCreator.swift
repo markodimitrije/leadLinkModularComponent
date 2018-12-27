8e00fbdbb2eced5da18f48526676c4c97c9f5f65
markodimitrije <markodimitrije@gmail.com>
2018-12-27 11:55:59 +0100

    refactor code iz vc fajla u sopstveni (signal input creator)

---
+++ tryLeadLinkModularComponent/ViewmodelInputCreator/ViewmodelInputCreator.swift
@@ -0,0 +1,9 @@
+//
+//  ViewmodelInputCreator.swift
+//  tryLeadLinkModularComponent
+//
+//  Created by Marko Dimitrijevic on 27/12/2018.
+//  Copyright © 2018 Marko Dimitrijevic. All rights reserved.
+//
+
+import Foundation
