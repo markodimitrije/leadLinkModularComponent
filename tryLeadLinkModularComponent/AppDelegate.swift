516e62f9fa44ffd5606956f2f42b4378b026d94b
Dimitrijevic Marko <markodimitrije@gmail.com>
2019-04-20 17:01:23 +0200

    check for question data (update only if changed)

--- tryLeadLinkModularComponent/AppDelegate.swift
+++ tryLeadLinkModularComponent/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private let realm = try! Realm.init()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //loadResources()
+        loadResources()
         print(Realm.Configuration.defaultConfiguration.fileURL!)
         return true
     }
@@ -31,8 +31,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let resource = Resource<Questions>(url: questionsURL)
         URLRequest.load(resource: resource)
             .subscribe(onNext: { [weak self] questions in
-                guard let strongSelf = self else {return}
+                
+                guard let strongSelf = self,
+                    let data = strongSelf.getData(from: questions),
+                    strongSelf.needsUpdate(data: data) else {
+                        print("update is not needed...");
+                        return
+                }
+                
+                UserDefaults.standard.set(data, forKey: "questions")
+                
                 let realmQuestions = questions.data.map { question -> RealmQuestion in
+                    
                     let realmQuestion = RealmQuestion.init()
                     realmQuestion.updateWith(question: question)
                     return realmQuestion
@@ -45,6 +55,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     .disposed(by: strongSelf.bag)
             }).disposed(by: bag)
     }
+    
+    private func getData<T: Codable>(from codable: T) -> Data? {
+        return try? JSONEncoder.init().encode(codable)
+    }
+    
+    private func needsUpdate(data: Data) -> Bool {
+        
+        if let oldData = UserDefaults.standard.value(forKey: "questions") as? Data {
+            return oldData != data
+        }
+        return true
+    }
 
 }
 
+//extension FileManager {
+//    static var documentDirectory: URL! {
+//        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//    }
+//    static var questionsDataUrl: URL! {
+//        return documentDirectory.appendingPathComponent("questions")
+//    }
+//}
